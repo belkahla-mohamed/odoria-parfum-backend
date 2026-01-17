7425e52ccfe9399ae00f322f4961b3ebdc24d3dd
belkahla-mohamed <dev.mohamedbelkahla@gmail.com>
2026-01-17 02:20:53 +0100

    ssl autorization

--- config/database.ts
+++ config/database.ts
@@ -1,15 +1,15 @@
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: env.bool('DATABASE_SSL', false),
-      schema: 'public',
+      connectionString: env("DATABASE_URL"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     pool: {
       min: 2,
       max: 10,
     },
-    acquireConnectionTimeout: 60000,
   },
 });
